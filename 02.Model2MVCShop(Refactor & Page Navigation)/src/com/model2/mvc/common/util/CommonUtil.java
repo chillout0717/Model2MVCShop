242c2735a0e44c2951cc25ff1164729ee74f64db
jehyunlim <jehyunlim@localhost>
2020-11-29 21:30:31 +0900

    create

---
+++ 02.Model2MVCShop(Refactor & Page Navigation)/src/com/model2/mvc/common/util/CommonUtil.java
@@ -0,0 +1,106 @@
+package com.model2.mvc.common.util;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class CommonUtil {
+	
+	///Field
+	
+	///Constructor
+	
+	///Method
+	public static String null2str(String org, String converted) {
+		if (org == null || org.trim().length() == 0)
+			return converted;
+		else
+			return org.trim();
+	}
+
+	public static String null2str(String org) {
+		return CommonUtil.null2str(org, "");
+	}
+
+	public static String null2str(Object org) {
+		if (org != null && org instanceof java.math.BigDecimal) {
+			return CommonUtil.null2str((java.math.BigDecimal) org, "");
+		} else {
+			return CommonUtil.null2str((String) org, "");
+		}
+	}
+
+	public static String null2str(java.math.BigDecimal org, String converted) {
+		if (org == null)
+			return converted;
+		else
+			return org.toString();
+	}
+
+	public static String null2str(java.math.BigDecimal org) {
+		return CommonUtil.null2str(org, "");
+	}
+
+	public static String toDateStr(String dateStr) {
+		if (dateStr == null)
+			return "";
+		else if (dateStr.length() != 8)
+			return dateStr;
+		else
+			return dateStr.substring(0, 4) + "/" + dateStr.substring(4, 6)
+					+ "/" + dateStr.substring(6, 8);
+	}
+
+	public static String toDateStr(Timestamp date) {
+		if (date == null)
+			return "";
+		else {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			return sdf.format(new Date(date.getTime()));
+		}
+	}
+
+	public static String toSsnStr(String ssnStr) {
+		if (ssnStr == null)
+			return "";
+		else if (ssnStr.length() != 13)
+			return ssnStr;
+		else
+			return ssnStr.substring(0, 6) + "-" + ssnStr.substring(6, 13);
+	}
+
+	public static String toAmountStr(String amountStr) {
+		String returnValue = "";
+		if (amountStr == null)
+			return returnValue;
+		else {
+			int strLength = amountStr.length();
+
+			if (strLength <= 3)
+				return amountStr;
+			else {
+				String s1 = "";
+				String s2 = "";
+				for (int i = strLength - 1; i >= 0; i--)
+					s1 += amountStr.charAt(i);
+
+				for (int i = strLength - 1; i >= 0; i--) {
+					s2 += s1.charAt(i);
+					if (i % 3 == 0 && i != 0)
+						s2 += ",";
+				}
+
+				return s2;
+			}
+		}
+	}
+
+	public static String toAmountStr(java.math.BigDecimal amount) { //소수점을 정확하게 표현하기 위한 메소드 더블보다 사용하는게 어렵고 느리지만 더블보다 더욱 정학하게 표현할수있음
+		if (amount == null) {
+			return "";
+		} else {
+			return toAmountStr(amount.toString());
+		}
+	}
+}
---
+++ 02.Model2MVCShop(Refactor & Page Navigation)/src/com/model2/mvc/framework/Action.java
@@ -0,0 +1,27 @@
+package com.model2.mvc.framework;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public abstract class Action {
+	
+	///Field
+	private ServletContext servletContext;
+	
+	///Constructor
+	public Action() {
+		System.out.println("액션에 도착했습니다");
+	}
+	
+	///Method
+	public ServletContext getServletContext() {
+		return servletContext;
+	}
+	public void setServletContext(ServletContext servletContext) {
+		this.servletContext = servletContext;
+	}
+
+	public abstract String execute(HttpServletRequest request, HttpServletResponse response) throws Exception ;
+}
